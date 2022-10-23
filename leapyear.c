a383441029581be1dfb586654fdbf68648605c00
Anshul407 <102428802+Anshul407@users.noreply.github.com>
2022-10-24 02:36:00 +0530

    Create leapyear.c

---
+++ leapyear.c
@@ -0,0 +1,21 @@
+//to find leap year
+#include<stdio.h>  
+int main()  
+{  
+    int year;  
+  
+    printf("Enter the year\n");  
+    scanf("%d", &year);  
+  
+    (year % 100 == 0) ?  
+    ( (year % 400 == 0)?  
+      (printf("%d is leap year\n", year)):  
+      (printf("%d is not leap year\n", year))  
+    ) :  
+    ( (year % 4 == 0)?  
+      (printf("%d is leap year\n", year)):  
+      (printf("%d is not leap year\n", year))  
+    );  
+  
+    return 0;  
+}  
